b3034e078a0031d024c8132d346793b76328b15c
NIkhitha07 <nikkibijjala@gmail.com>
2025-01-27 15:24:08 -0500

    Fixed routing issues for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -5,53 +5,52 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
 import About from "./components/About";
-
+import Testimonials from "./components/Testimonials";
 import Profile from "./img/Nikhitha.jpg";
-import Testimonials from "./components/Testimonals";
 
 function App() {
   return (
-    <div>
-      <div className="mainNav">
-        <nav className="navbar" id="myLinks">
-          <div className="imgSection nav-logo-wrapper">
-            <div>
-              <img className="profile" src={Profile} alt="profile" />
-            </div>
-            <div>
-              <p>Nikhitha Bijjala</p>
-              <div className="logo-tagline">
-                Full Stack Developer | Programmer
+    <Router basename="/Nikhitha-Portfolio"> 
+      <div>
+        <div className="mainNav">
+          <nav className="navbar" id="myLinks">
+            <div className="imgSection nav-logo-wrapper">
+              <div>
+                <img className="profile" src={Profile} alt="profile" />
+              </div>
+              <div>
+                <p>Nikhitha Bijjala</p>
+                <div className="logo-tagline">
+                  Full Stack Developer | Programmer
+                </div>
               </div>
             </div>
-          </div>
-          <div>
-            <Link to="/" className="navbar-link">
-              Home
-            </Link>
-
-            <Link to="/experience" className="navbar-link">
-              Experience
-            </Link>
-            <Link to="/testimonals" className="navbar-link">
-              Testimonals
-            </Link>
-
-            <Link to="/contact" className=" touch-btn">
-              Get in touch
-            </Link>
-          </div>
-        </nav>
+            <div>
+              <Link to="/" className="navbar-link">
+                Home
+              </Link>
+              <Link to="/experience" className="navbar-link">
+                Experience
+              </Link>
+              <Link to="/testimonials" className="navbar-link">
+                Testimonials
+              </Link>
+              <Link to="/contact" className="touch-btn">
+                Get in touch
+              </Link>
+            </div>
+          </nav>
+        </div>
+        <Routes className="section1">
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/testimonials" element={<Testimonials />} />
+        </Routes>
       </div>
-      <Routes className="section1">
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/testimonals" element={<Testimonials />} />
-      </Routes>
-    </div>
+    </Router>
   );
 }
 
